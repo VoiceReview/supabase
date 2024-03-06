51e532a81c0511523e1c2ba75e0bf7fff9fd1345
Thomas Duclos <thomasduclos1012@gmail.com>
2024-03-06 08:37:35 +0100

    feat(test-app): users can sign in with google and be redirected to dashboard

--- test-app/pages/_document.tsx
+++ test-app/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="m-0">
       <Head />
-      <body>
+      <body className="h-screen w-screen">
         <Main />
         <NextScript />
       </body>
