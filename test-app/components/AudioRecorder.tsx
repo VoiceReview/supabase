78ae07d46beeeefa141021c3d62417a4d4108055
BragdonD <thomasduclos1012@gmail.com>
2024-03-07 17:21:20 +0100

    feat(test-app): example of how to use supabase storage

--- test-app/components/AudioRecorder.tsx
+++ test-app/components/AudioRecorder.tsx
@@ -1,23 +1,28 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 
-// On phone, the user will have to keep the button pressed to record 
+// On phone, the user will have to keep the button pressed to record
 // and release it to stop recording.
-// On desktop, the user will have to click the button to start recording 
+// On desktop, the user will have to click the button to start recording
 // and click it again to stop recording.
 
-const AudioRecorder = () => {
+const AudioRecorder = ({
+  onBlobReady,
+}: {
+  onBlobReady: (blob: Blob) => void;
+}) => {
   const [isRecording, setIsRecording] = useState(false);
   const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
-  const [audioUrl, setAudioUrl] = useState('');
+  const [audioUrl, setAudioUrl] = useState("");
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
 
   const startRecording = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.ondataavailable = event => {
+      mediaRecorder.ondataavailable = (event) => {
         const audioBlob = event.data;
         setAudioBlob(audioBlob);
+        onBlobReady(audioBlob);
         const audioUrl = URL.createObjectURL(audioBlob);
         setAudioUrl(audioUrl);
       };
@@ -25,7 +30,7 @@ const AudioRecorder = () => {
       setIsRecording(true);
       mediaRecorderRef.current = mediaRecorder;
     } catch (err) {
-      console.error('Could not start recording', err);
+      console.error("Could not start recording", err);
     }
   };
 
@@ -41,11 +46,21 @@ const AudioRecorder = () => {
       <h2 className="text-lg font-semibold mb-4">Laissez un avis vocal</h2>
       <div>
         {isRecording ? (
-          <button onClick={stopRecording} className="bg-red-500 text-white p-2 rounded">Arrêter l'enregistrement</button>
+          <button
+            onClick={stopRecording}
+            className="bg-red-500 text-white p-2 rounded"
+          >
+            Arrêter l'enregistrement
+          </button>
         ) : (
-          <button onClick={startRecording} className="bg-blue-500 text-white p-2 rounded">Commencer l'enregistrement</button>
+          <button
+            onClick={startRecording}
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Commencer l'enregistrement
+          </button>
         )}
-        {audioUrl && <audio src={audioUrl} controls className="mt-4"/>}
+        {audioUrl && <audio src={audioUrl} controls className="mt-4" />}
       </div>
     </section>
   );
