944e4b8d2b344893df65071eea6f3c152866c61a
BragdonD <thomasduclos1012@gmail.com>
2024-03-08 21:06:23 +0100

    feat(supabase/functions): insert helpers functions

--- supabase/migrations/20240307151733_Comment-supabase.sql
+++ supabase/migrations/20240307151733_Comment-supabase.sql
@@ -1,23 +1,24 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE Comment (
-  Comment_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE comments (
+  comment_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   type VARCHAR(255) NOT NULL,
-  user_id UUID NOT NULL,
+  user_id UUID NOT NULL REFERENCES auth.users (id) ON DELETE CASCADE,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
   deleted_at TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE Audio (
-  Audio_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  Comment_id UUID NOT NULL,
+CREATE TABLE audios (
+  audio_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  comment_id UUID NOT NULL,
   url VARCHAR(255) NOT NULL,
-  FOREIGN KEY (Comment_id) REFERENCES Comment (Comment_id) ON DELETE CASCADE
+  FOREIGN KEY (comment_id) REFERENCES comments (comment_id) ON DELETE CASCADE
 );
 
-CREATE TABLE Text (
-  Text_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  Comment_id UUID NOT NULL,
+CREATE TABLE texts (
+  text_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  comment_id UUID NOT NULL,
   content TEXT NOT NULL,
-  FOREIGN KEY (Comment_id) REFERENCES Comment (Comment_id) ON DELETE CASCADE
+  FOREIGN KEY (comment_id) REFERENCES comments (comment_id) ON DELETE CASCADE
 );
