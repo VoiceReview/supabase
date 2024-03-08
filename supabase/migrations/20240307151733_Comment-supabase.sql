692a8425c94c64b4cf9c2286c40e1e060eb48db6
LilKotei <ibrahim.kebe@edu.ece.fr>
2024-03-08 15:22:18 +0100

    chore(migration): adding relationships

--- supabase/migrations/20240307151733_Comment-supabase.sql
+++ supabase/migrations/20240307151733_Comment-supabase.sql
@@ -1,21 +1,23 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE Comment (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  Comment_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   type VARCHAR(255) NOT NULL,
   user_id UUID NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  deleted_at TIMESTAMP WITH TIME ZONE,
-  parent_id UUID NOT NULL,
-  child_id UUID NOT NULL
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE Audio (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    url VARCHAR(255) NOT NULL
+  Audio_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  Comment_id UUID NOT NULL,
+  url VARCHAR(255) NOT NULL,
+  FOREIGN KEY (Comment_id) REFERENCES Comment (Comment_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Text (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    content TEXT NOT NULL
+  Text_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  Comment_id UUID NOT NULL,
+  content TEXT NOT NULL,
+  FOREIGN KEY (Comment_id) REFERENCES Comment (Comment_id) ON DELETE CASCADE
 );
