957999b8d3039f5d5f6e11acdb70f34c9c8c56e9
BragdonD <thomasduclos1012@gmail.com>
2024-03-08 21:06:23 +0100

    feat(supabase/functions): insert helpers functions

--- supabase/functions/comments/routes/audioRoutes.ts
+++ supabase/functions/comments/routes/audioRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.7";
+import { Database } from "../../_shared/database.type.ts";
 
 const audioRouter = new Router();
 
 audioRouter
-    .get("/audio",  (ctx) => {
-        ctx.response.body = 'This is an example Oak server running on Edge Functions!';
+    .get("/comments/audio/healtcheck", (ctx) => {
+        ctx.response.body = "Audio service is up and running";
     })
-    .get("/audio/:id", async (ctx) => {     
-        const supabaseClient = createClient(
+    .get("/comments/audio/:id", async (ctx) => {     
+        const supabaseClient = createClient<Database>(
             Deno.env.get("SUPABASE_URL") ?? '',
             Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? '',
             {
@@ -34,5 +35,26 @@ audioRouter
             error
         };
     })
+    .post("/comments/audio", async (ctx) => {
+        const supabaseClient = createClient<Database>(
+            Deno.env.get("SUPABASE_URL") ?? '',
+            Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? '',
+            {
+                global: {
+                    headers: {
+                        Authorization: ctx.request.headers.get('Authorization')!,
+                    }
+                }
+            }
+        );
+
+        //insertComment(supabaseClient, )
+
+        // ctx.response.body = {
+        //     data,
+        //     error
+        // };
+    });
+        
 
 export default audioRouter;
