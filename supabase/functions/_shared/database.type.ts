5ea85ba971c5580f5b88169a6a413c4bc562adc8
Thomas Duclos <thomasduclos1012@gmail.com>
2024-03-12 09:55:49 +0100

    feat(supabase/functions): add likes functionality to the comments

--- supabase/functions/_shared/database.type.ts
+++ supabase/functions/_shared/database.type.ts
@@ -34,7 +34,7 @@ export type Database = {
   }
   public: {
     Tables: {
-      audios: {
+      audio: {
         Row: {
           audio_id: string
           comment_id: string
@@ -52,42 +52,51 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "audios_comment_id_fkey"
+            foreignKeyName: "audio_comment_id_fkey"
             columns: ["comment_id"]
             isOneToOne: false
-            referencedRelation: "comments"
+            referencedRelation: "comment"
             referencedColumns: ["comment_id"]
           },
         ]
       }
-      comments: {
+      comment: {
         Row: {
           comment_id: string
           created_at: string | null
           deleted_at: string | null
+          nb_child: number | null
+          nb_dislike: number | null
+          nb_like: number | null
+          parent_id: string | null
           type: string
-          updated_at: string | null
           user_id: string
         }
         Insert: {
           comment_id?: string
           created_at?: string | null
           deleted_at?: string | null
+          nb_child?: number | null
+          nb_dislike?: number | null
+          nb_like?: number | null
+          parent_id?: string | null
           type: string
-          updated_at?: string | null
-          user_id: string
+          user_id?: string
         }
         Update: {
           comment_id?: string
           created_at?: string | null
           deleted_at?: string | null
+          nb_child?: number | null
+          nb_dislike?: number | null
+          nb_like?: number | null
+          parent_id?: string | null
           type?: string
-          updated_at?: string | null
           user_id?: string
         }
         Relationships: [
           {
-            foreignKeyName: "comments_user_id_fkey"
+            foreignKeyName: "comment_user_id_fkey"
             columns: ["user_id"]
             isOneToOne: false
             referencedRelation: "users"
@@ -95,7 +104,46 @@ export type Database = {
           },
         ]
       }
-      texts: {
+      reaction: {
+        Row: {
+          comment_id: string
+          created_at: string | null
+          like_id: string
+          like_type: boolean
+          user_id: string
+        }
+        Insert: {
+          comment_id: string
+          created_at?: string | null
+          like_id?: string
+          like_type: boolean
+          user_id?: string
+        }
+        Update: {
+          comment_id?: string
+          created_at?: string | null
+          like_id?: string
+          like_type?: boolean
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "reaction_comment_id_fkey"
+            columns: ["comment_id"]
+            isOneToOne: false
+            referencedRelation: "comment"
+            referencedColumns: ["comment_id"]
+          },
+          {
+            foreignKeyName: "reaction_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      text: {
         Row: {
           comment_id: string
           content: string
@@ -113,10 +161,10 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: "texts_comment_id_fkey"
+            foreignKeyName: "text_comment_id_fkey"
             columns: ["comment_id"]
             isOneToOne: false
-            referencedRelation: "comments"
+            referencedRelation: "comment"
             referencedColumns: ["comment_id"]
           },
         ]
@@ -320,80 +368,81 @@ type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
-  | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-  | { schema: keyof Database },
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-    Database[PublicTableNameOrOptions["schema"]]["Views"])
-  : never = never,
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-    Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
-  ? R
-  : never
+    ? R
+    : never
   : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-    PublicSchema["Views"])
-  ? (PublicSchema["Tables"] &
-    PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-  ? R
-  : never
-  : never
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-  | keyof PublicSchema["Tables"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-  : never = never,
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Insert: infer I
-  }
-  ? I
-  : never
+      Insert: infer I
+    }
+    ? I
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-    Insert: infer I
-  }
-  ? I
-  : never
-  : never
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-  | keyof PublicSchema["Tables"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-  : never = never,
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-    Update: infer U
-  }
-  ? U
-  : never
+      Update: infer U
+    }
+    ? U
+    : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-  ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-    Update: infer U
-  }
-  ? U
-  : never
-  : never
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
-  | keyof PublicSchema["Enums"]
-  | { schema: keyof Database },
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-  : never = never,
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-  ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-  : never
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
